cdab7c2245b9a983b6f728e8d2034a31a04c7b21
thatcharles <charles5j21z@gmail.com>
2017-04-03 22:01:19 +0800

    SD_gradeSystem copy is Jean's update

---
+++ SD_gradeSystem copy/src/Loggin.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Loggin{
+	public String input;
+	
+	public void requestID(ArrayList<String> S) {
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+	    System.out.println("��JID�� Q (�����ϥ�)�H"); //輸入ID或 Q (結束使用)？
+	    String input = reader.next();//input = ID or Q.
+	    
+	    if(input.equals("Q"))	 System.out.println("�����{��");
+        else	searchID(S);
+	}
+	
+    
+    private void searchID(ArrayList<String> S){
+    	int flag=1;
+       //input = ID.
+    	for(int i=0;i<S.size();i++){
+    		if(S.get(i).equals(input)){
+    			MainTable  choice= new MainTable();
+    			choice.commandTable(S,i);
+    			flag=0;
+    			break;
+    		}
+    	}
+    	if(flag==1) 	System.out.println("���򳣨S��");
+        
+    }
+
+
+
+
+
+    
+}
+
